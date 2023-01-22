95efb597ba15d19a6364f3bde8d28aac1f66f240
MagnuZvn <lionegmanuel@gmail.com>
2023-01-22 19:43:09 -0300

    optimizacion y version final encriptador

--- challenge1.js
+++ challenge1.js
@@ -14,7 +14,7 @@ function validarinput(textref) {
         return true;    
     }
 }
-function validarInputEncripted(textref1, textref2) {
+function validarInputResultante(textref1, textref2) { //funcion para validad que los textos objetivos no sean iguales
     if (textref1!=textref2) { // si el resultado del desencriptado es diferente, entonces se puede mostrar dicho resultado, de lo contrario, se lanza un mensaje.
         return true;
     }
@@ -22,6 +22,7 @@ function validarInputEncripted(textref1, textref2) {
         return false;
     }
 }
+
 function replacesEncripted(inputTextRef, newTextRef ) { //funcion que reemplaza los caracteres por sus llaves en ENCRIPTADO
     for (let i=0; i<inputTextRef.length;i++) {
         let letter = inputTextRef.charAt(i); //actualizacion de la letra a recorrer de forma constante
@@ -62,35 +63,65 @@ function replacesDesencripted(inputTextRef, newTextRef ) { //funcion que reempla
 button1.addEventListener("click",encriptar); //evento para encriptar
 function encriptar() { //si aun no se encripto, el valor booleano sera verdadero //ejemplo: hola como estas = hoberlai cobermober enterstars   
         let inputText = document.getElementById("inputText").value; //texto de ingreso por parte del usuario
-        //proceso de cambio (encriptacion)
-        if (validarinput(inputText)) {
-            let arbitraryText = ""; //cadena vacia para posteriormente concatenar con cada caracter correspondiente
-            let encriptedText = replacesEncripted(inputText, arbitraryText);
-            alert("¡TEXTO ENCRIPTADO!");    
-            const resultado = document.getElementById("textoresultado");
-            resultado.innerHTML = encriptedText;
-            document.getElementById("inputText").value =""; //reset del textarea de ingreso
-        }
+            if (document.getElementById("textoresultado").value=="") {
+                if (validarinput(inputText)) {
+                    let arbitraryText = ""; //cadena vacia para posteriormente concatenar con cada caracter correspondiente
+                    let encriptedText1 = replacesEncripted(inputText, arbitraryText);
+                    alert("¡TEXTO ENCRIPTADO!");    
+                    const resultado = document.getElementById("textoresultado");
+                    resultado.innerHTML = encriptedText1;
+                    document.getElementById("inputText").value =""; //reset del textarea de ingreso
+                }  
+            }
+            else {
+                let resultadoDesencriptado = document.getElementById("textoresultado").value; //acceso al texto ya deseencriptado
+                arbitraryText = "";
+                let encriptedText = replacesEncripted(resultadoDesencriptado, arbitraryText);
+                let result2 = validarInputResultante(resultadoDesencriptado, encriptedText); 
+                if (result2) {
+                    let result1 = confirm("!EL SIGUIENTE TEXTO SERA ENCRIPTADO!\n"+resultadoDesencriptado);
+                    if (result1) {
+                        alert("¡TEXTO ENCRIPTADO!");
+                        const encriptedresult = document.getElementById("textoresultado");
+                        encriptedresult.innerHTML = encriptedText;
+                    }
+                }
+            }
+        
+        
+       
 }
 button2.addEventListener("click",desencriptar); //evento para desencriptar
 function desencriptar() {
-        let resultadoEncriptado = document.getElementById("textoresultado").value; //acceso al texto ya encriptado
-        //proceso de desencriptado
-        arbitraryText = "";
-        let desencriptedText = replacesDesencripted(resultadoEncriptado, arbitraryText);
-        let result2 = validarInputEncripted(resultadoEncriptado, desencriptedText);
-        if (result2) {
-            let result1 = confirm("!EL SIGUIENTE TEXTO SERA DESENCRIPTADO!\n"+resultadoEncriptado);
-            if (result1) {
-            alert("¡TEXTO DESENCRIPTADO!");
-            const desencriptedresult = document.getElementById("textoresultado");
-            desencriptedresult.innerHTML = desencriptedText;
-        }       
-        }
-        else {
-            alert("¡ERROR!\nEL TEXTO RESULTANTE NO SE PUEDE DESENCRIPTAR");
-        }  
-        
+        let inputText = document.getElementById("inputText").value; //compruebo si es el texto que va a ingresar el usuario
+            if (document.getElementById("textoresultado").value=="") {
+                if (validarinput(inputText)) {
+                    let arbitraryText1 = ""; 
+                    let desencriptedText1 = replacesDesencripted(inputText, arbitraryText1);
+                    alert("TEXTO DESENCRIPTADO!");
+                    const result = document.getElementById("textoresultado");
+                    result.innerHTML = desencriptedText1;
+                    document.getElementById("inputText").value = ""; 
+                }      
+            }
+            else {
+                let resultadoEncriptado = document.getElementById("textoresultado").value; //acceso al texto ya encriptado
+                arbitraryText = "";
+                let desencriptedText = replacesDesencripted(resultadoEncriptado, arbitraryText);
+                let result2 = validarInputResultante(resultadoEncriptado, desencriptedText); //confirmacion para saber si los textos son iguales o no, ya que si el resultado desencriptado es igual, entonces no se "puede" desencriptar
+                if (result2) {
+                    let result1 = confirm("!EL SIGUIENTE TEXTO SERA DESENCRIPTADO!\n"+resultadoEncriptado);
+                    if (result1) {
+                        alert("¡TEXTO DESENCRIPTADO!");
+                        const desencriptedresult = document.getElementById("textoresultado");
+                        desencriptedresult.innerHTML = desencriptedText;
+                    }       
+                }
+                else {
+                    alert("¡ERROR!\nEL TEXTO RESULTANTE NO PUEDE SER DESENCRIPTADO");  
+                }
+            }
+                   
 }
 
 function reiniciar() {
